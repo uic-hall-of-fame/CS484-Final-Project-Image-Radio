cb3c924bb6ab010555889ebc67b135c4468baafe
Jason10897 <jasondsouza10897@gmail.com>
2022-11-05 20:17:39 -0500

    Add architecture diagram to About page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,43 +1,50 @@
-import { Container } from "@mui/system";
-import React from "react";
-import { Typography } from "@mui/material";
-import CircleIcon from "@mui/icons-material/Circle";
+import React from 'react';
+import { Typography } from '@mui/material';
+import Box from '@mui/material/Box';
 
 export default function About() {
     return (
         <>
             <Typography
-                variant="h4"
-                component="div"
+                variant='h4'
+                component='div'
                 gutterBottom
-                align="center"
+                align='center'
                 sx={{ mt: 4 }}
             >
                 Final project Tech stack and MVP proposal
             </Typography>
 
             <Typography
-                variant="h5"
-                component="div"
+                variant='h5'
+                component='div'
                 gutterBottom
-                sx={{ mt: 4, ml: 4, textDecoration: "underline" }}
+                sx={{ mt: 4, ml: 4, textDecoration: 'underline' }}
             >
-                Tech Stack Diagram
+                Architecture Diagram
             </Typography>
 
-            {/* Paste image here */}
+            <Box display='flex' justifyContent='center' alignItems='center'>
+                <img
+                    alt='Architecture Diagram'
+                    src='/images/Architecture_Diagram.png'
+                />
+            </Box>
 
             <Typography
-                variant="h5"
-                component="div"
+                variant='h5'
+                component='div'
                 gutterBottom
-                sx={{ mt: 4, ml: 4, textDecoration: "underline" }}
+                sx={{ mt: 4, ml: 4, textDecoration: 'underline' }}
             >
-                MVP(Minimum Viable Product) description
+                MVP (Minimum Viable Product) Description
             </Typography>
 
-            <Typography variant="h6" gutterBottom sx={{ ml: 4 }}>
-                We are planning to play and display the lyrics of songs in sync. We will be using Spotify API for authorizing users, fetching lyrics of the songs, and playing songs. Supabase database will be used for backend while React will be used for Frontend. 
+            <Typography variant='h6' gutterBottom sx={{ ml: 4 }}>
+                We are planning to play and display the lyrics of songs in sync.
+                We will be using Spotify API for authorizing users, fetching
+                lyrics of the songs, and playing songs. Supabase database will
+                be used for backend while React will be used for Frontend.
             </Typography>
         </>
     );
