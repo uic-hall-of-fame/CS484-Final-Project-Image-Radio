8fbc1a4b9be7274e2ac5a48632eea5d2d6c214d3
Jason10897 <jasondsouza10897@gmail.com>
2022-11-08 22:09:05 -0600

    Add toggle for user menu

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -62,47 +62,6 @@ function Navbar({ supabase, session = null }) {
                     Image Radio
                 </Typography>
 
-                <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-                    <IconButton
-                        size="large"
-                        aria-label="account of current user"
-                        aria-controls="menu-appbar"
-                        aria-haspopup="true"
-                        onClick={handleOpenNavMenu}
-                        color="inherit"
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                    <Menu
-                        id="menu-appbar"
-                        anchorEl={anchorElNav}
-                        anchorOrigin={{
-                            vertical: 'bottom',
-                            horizontal: 'left',
-                        }}
-                        keepMounted
-                        transformOrigin={{
-                            vertical: 'top',
-                            horizontal: 'left',
-                        }}
-                        open={Boolean(anchorElNav)}
-                        onClose={handleCloseNavMenu}
-                        sx={{
-                            display: { xs: 'block', md: 'none' },
-                        }}
-                    >
-                        {pages.map((page) => (
-                            <MenuItem
-                                key={page}
-                                onClick={handleCloseNavMenu}
-                            >
-                                <Typography textAlign="center">
-                                    {page}
-                                </Typography>
-                            </MenuItem>
-                        ))}
-                    </Menu>
-                </Box>
                 <Box sx={{ flexGrow: 1, mt: 0.5 }}>
                     {pages.map((page) => (
                         <Button
@@ -116,27 +75,25 @@ function Navbar({ supabase, session = null }) {
                 </Box>
 
                 <Box sx={{ flexGrow: 0 }}>
-                    <Tooltip title="Open settings">
-                        <IconButton
-                            onClick={handleOpenUserMenu}
-                            sx={{
-                                p: 0,
-                                mr: '10px',
-                                color: 'white',
-                                fontSize: '20px',
-                            }}
-                        >
-                            {session ? (
-                                <Avatar
-                                    alt="Remy Sharp"
-                                    src="/static/images/user.jpg"
-                                    sx={{ mr: '10px' }}
-                                />
-                            ) : (
-                                <>Login</>
-                            )}
-                        </IconButton>
-                    </Tooltip>
+                    <IconButton
+                        onClick={session ? handleOpenUserMenu : null}
+                        sx={{
+                            p: 0,
+                            mr: '10px',
+                            color: 'white',
+                            fontSize: '20px',
+                        }}
+                    >
+                        {session ? (
+                            <Avatar
+                                alt="Remy Sharp"
+                                src="/static/images/user.jpg"
+                                sx={{ mr: '10px' }}
+                            />
+                        ) : (
+                            <>Login</>
+                        )}
+                    </IconButton>
                     <Menu
                         sx={{ mt: '45px' }}
                         id="menu-appbar"
