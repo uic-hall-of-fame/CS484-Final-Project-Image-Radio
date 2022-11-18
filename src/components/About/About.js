4badbe3b3a458cd098d4acea057b4d2c8fe72930
Jason10897 <jasondsouza10897@gmail.com>
2022-11-18 15:48:25 -0600

    Add final submission description to the About page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -49,14 +49,40 @@ export default function About() {
                 sx={{ ml: 4 }}
             >
                 We plan to play a song and display its lyrics in a synchronous
-                manner. We will use <b>OAuth with Spotify</b> for user login and
-                to access user playlists. After this, we will fetch the
-                timestamped lyrics of the song and play it using the{' '}
-                <b>Spotify API</b>.<b>Supabase</b> database will be used for the
-                backend, while
-                <b>React</b> will be used for the frontend. We will use
-                <b>GitHub</b> for version control along with <b>Netlify</b> for
-                CI and hosting.
+                manner. We will use <b>OAuth with Spotify</b> for user login.
+                After this, we will fetch the timestamped lyrics of the song and
+                play it using the <b>Spotify API</b>. <b>Supabase</b> database
+                will be used for the backend, while <b>React</b> will be used
+                for the frontend. We will use <b>GitHub</b> for version control
+                along with <b>Netlify</b> for CI and hosting. We successfully
+                completed all the planned tasks for the MVP.
+            </Typography>
+
+            <Typography
+                variant="h5"
+                component="div"
+                gutterBottom
+                sx={{ mt: 4, ml: 4, textDecoration: 'underline' }}
+            >
+                Final Submission Description
+            </Typography>
+
+            <Typography
+                variant="h6"
+                gutterBottom
+                sx={{ ml: 4 }}
+            >
+                We plan to play a song and display AI generated images from its
+                lyrics, in a synchronous manner. We will use{' '}
+                <b>OAuth with Spotify</b> for user login. After this, we will
+                fetch the timestamped lyrics of the song and play it using the{' '}
+                <b>Spotify API</b>. <b>Supabase</b> database will be used for
+                the backend, while <b>React</b> will be used for the frontend.
+                We will use <b>GitHub</b> for version control along with{' '}
+                <b>Netlify</b> for CI and hosting. Our project also utilizes an
+                image generating AI API (probably the newly released{' '}
+                <b>DALLE-2</b>), to generate images based on the lyrics of the
+                song.
             </Typography>
         </>
     );
