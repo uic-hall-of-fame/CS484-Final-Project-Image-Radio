64ec19981c8441dec2b5ebb83935201b000d5bdf
Jason10897 <jasondsouza10897@gmail.com>
2022-12-03 00:44:17 -0600

    Modify About page content

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -58,8 +58,9 @@ export default function About() {
 
             <Typography
                 variant="h6"
+                align="justify"
                 gutterBottom
-                sx={{ ml: 4 }}
+                sx={{ ml: 4, mr: 4 }}
             >
                 We plan to play a song and display its lyrics, in a synchronous
                 manner. We will use <b>OAuth with Spotify</b> for user login.
@@ -89,8 +90,9 @@ export default function About() {
 
             <Typography
                 variant="h6"
+                align="justify"
                 gutterBottom
-                sx={{ ml: 4, mb: 4 }}
+                sx={{ ml: 4, mr: 4, mb: 4 }}
             >
                 We plan to play a song and display AI generated images from its
                 lyrics, in a synchronous manner. We will use{' '}
@@ -99,10 +101,10 @@ export default function About() {
                 <b>Spotify API</b>. <b>Supabase</b> database will be used for
                 the backend, while <b>React</b> will be used for the frontend.
                 We will use <b>GitHub</b> for version control along with{' '}
-                <b>Netlify</b> for CI and hosting. Our project will also utilize
-                an image generating AI API (probably the newly released{' '}
-                <b>DALLE-2</b>), to generate images based on the lyrics of the
-                song.
+                <b>Netlify</b> for CI and hosting. Our project also utilizes the
+                newly released <b>DALLE-2</b> API, to generate images based on
+                the lyrics of the song. We successfully completed all the
+                planned tasks for the final submission.
             </Typography>
         </>
     );
